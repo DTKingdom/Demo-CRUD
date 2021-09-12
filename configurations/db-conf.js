db9c23b5d2cb4de3ffb19e5516736bd642d74587
DTKingdom <dylantobar2001@gmail.com.com>
2021-09-12 16:52:33 -0600

    make it better

--- configurations/db-conf.js
+++ configurations/db-conf.js
@@ -1,7 +1,7 @@
-    const host= "localhost";
-    const user= "root";
-    const password= "1234";
-    const database= "escuela";
+    const host= "y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
+    const user= "dyc49hxe9vxlzzi6";
+    const password= "p1u6pjb0m7lc10fe";
+    const database= "o40psjtoqny37wqz";
 
     exports.host=host;
     exports.user=user;
